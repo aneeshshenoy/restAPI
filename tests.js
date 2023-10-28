4646c0a3e6e5d8f046ab21afcd3a74b007ca9b8c
Aneesh Shenoy <67361724+aneeshshenoy@users.noreply.github.com>
2023-10-28 18:44:22 +0530

    tests.js
    tests done here with the help of chai and mocha (test code not implemented for the user operations as specified

---
+++ tests.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('User Profile API', () => {
+    it('return JWT token when user logs in', (done) => {
+
+    });
+
+    it('create user profile after authentication', (done) => {
+
+    });
+
+    it('should return a list of user profiles after pagination and sorting', (done) => {
+
+    });
+
+    it('should update a user profile after authentication and autherization', (done) => {
+
+    });
+
+    it('should delete a user profile after authenticatio and autherization' (done) => {
+
+    });
+
+});
